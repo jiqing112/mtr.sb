8fe31fb0b6bf66a0d50ad009a9beb064b2f8772c
imlonghao <git@imlonghao.com>
2023-05-09 23:02:42 +0800

    docs: ✏️ update index page

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,6 +1,8 @@
 export default function Home() {
   return <>
     <h1>Index</h1>
-    <p>TBD...</p>
+    <p>A free network diagnostic tool.</p>
+    <h2>Contact</h2>
+    <p>support #at# mtr.sb</p>
   </>
 }
